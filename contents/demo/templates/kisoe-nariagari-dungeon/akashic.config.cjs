4f412c60c33f8970c192f243cdbeab372a242623
ShinobuTakahashi <a.little.more.than.before@gmail.com>
2025-04-02 16:08:03 +0900

    Use custom font in

---
+++ contents/demo/templates/kisoe-nariagari-dungeon/akashic.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    commandOptions: {
+      serve: {
+        fonts: [
+          {
+            path: "../../../../font/MPLUSRounded1c-Regular.ttf",
+            descriptors: {
+              "font-family": "MPLUSRounded1c-Regular"
+            },
+          }
+        ]
+      }
+    }
+  };
